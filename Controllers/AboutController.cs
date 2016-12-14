d262ebb9e35e48f7833f3e2a13c91711d4128580
Fredrik <fredrikostman85@gmail.com>
2016-12-14 22:48:36 +0100

    First commit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutIda()
+        {
+            return View();
+        }
+
+        public ActionResult AboutHille()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EnglishController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteProject.Controllers
+{
+    public class EnglishController : Controller
+    {
+        // GET: English
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
